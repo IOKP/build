0a7f972c7dbbcf54a735f18a3697233a5890a649
mrimp <mrimp@day-off.net>
2013-11-24 08:27:53 -0500

    Fix envsetup

--- envsetup.sh
+++ envsetup.sh
@@ -644,7 +644,7 @@ function tapas()
 function eat()
 {
     if [ "$OUT" ] ; then
-        MODVERSION=`sed -n -e'/ro\.aokp\.version/s/^.*=//p' $OUT/system/build.prop`
+        MODVERSION=`sed -n -e'/ro\.iokp\.version/s/^.*=//p' $OUT/system/build.prop`
         ZIPFILE=$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
